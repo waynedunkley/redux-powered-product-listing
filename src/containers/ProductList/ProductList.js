e76b314e6a9a6bf417b8c2042b48075b9ac6025b
Wayne Dunkley <dev@waynedunkley.com>
2018-09-16 17:44:01 +1000

    Add ProductList container

---
+++ src/containers/ProductList/ProductList.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styled from "styled-components"
+import divStyles from "./Header.style"
+
+const StyledDiv = styled.div`
+  ${divStyles};
+`
+
+const ProductList = () => <StyledDiv />
+
+export default ProductList
---
+++ src/containers/ProductList/ProductList.style.js
@@ -0,0 +1,6 @@
+const divStyles = ({ theme: { colors } }) => `
+  display: flex;
+  justify-content: space-between;
+`
+
+export default divStyles
