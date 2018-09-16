1fb1fbae57557c03e2941b53c993af0d36ee2988
Wayne Dunkley <dev@waynedunkley.com>
2018-09-16 16:29:38 +1000

    Add Header container

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Container } from "components"
+
+const Header = () => (
+  <Container>
+    <p>Header</p>
+  </Container>
+)
+
+export default Header
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,3 @@
 export { default as App } from "./App/App"
 export { default as Home } from "./Home/Home"
+export { default as Header } from "./Header/Header"
