a9439386d9966cb9436efa1b5cafa37ef43eaf03
Wayne Dunkley <dev@waynedunkley.com>
2018-09-17 23:14:03 +1000

    remove service workers

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { App } from "containers";
-import store from "./redux/store";
-// import registerServiceWorker from './registerServiceWorker'
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { App } from "containers"
+import store from "./redux/store"
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById("root")
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  rootEl
-);
+  rootEl,
+)
 
 if (module.hot) {
   module.hot.accept("./index", () => {
     // eslint-disable-next-line global-require
-    const NextApp = require("./index").default;
+    const NextApp = require("./index").default
 
-    ReactDOM.render(<NextApp />, rootEl);
-  });
+    ReactDOM.render(<NextApp />, rootEl)
+  })
 }
-
-// registerServiceWorker()
