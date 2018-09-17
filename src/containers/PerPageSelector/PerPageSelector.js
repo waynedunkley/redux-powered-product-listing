dc93964e6ef427b12968717acf1ae1e6191d9b53
Wayne Dunkley <dev@waynedunkley.com>
2018-09-18 09:08:50 +1000

    Simplify pageSelector

--- src/containers/PerPageSelector/PerPageSelector.js
+++ src/containers/PerPageSelector/PerPageSelector.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { connect } from "react-redux"
 import styled from "styled-components"
 import { updatePerPage } from "redux/modules/products"
@@ -8,12 +9,18 @@ const StyledSelect = styled.select`
   ${selectStyles};
 `
 
+const propTypes = {
+  dispatch: PropTypes.func.isRequired,
+}
+
+const values = [8, 16, 32]
+
 const PerPageSelector = ({ dispatch }) => (
   <StyledSelect onChange={e => dispatch(updatePerPage(e.currentTarget.value))}>
-    <option value="8">8 per page</option>
-    <option value="16">16 per page</option>
-    <option value="32">32 per page</option>
+    {values && values.map(v => <option value={v}>{v} per page</option>)}
   </StyledSelect>
 )
+
+PerPageSelector.propTypes = propTypes
 
 export default connect()(PerPageSelector)
